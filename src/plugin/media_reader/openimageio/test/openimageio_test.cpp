c280a0066a66c91ea395d8e6fe3b2c79963261cc
Thomas Manceau <manceau.thomas19@gmail.com>
2025-10-25 01:42:52 +0200

    Implement OIIO reader
    Signed-off-by: Thomas Manceau <manceau.thomas19@gmail.com>
    Signed-off-by: Sam.Richards@taurich.org <Sam.Richards@taurich.org>

---
+++ src/plugin/media_reader/openimageio/test/openimageio_test.cpp
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+#include "xstudio/utility/helpers.hpp"
+
+using namespace xstudio::utility;
+
+GTEST_TEST(OIIOMediaReaderTest, Test) {
+    // Simple test to verify that the plugin compiles
+    EXPECT_TRUE(true);
+}
+
