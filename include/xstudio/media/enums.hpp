cba2aa47350b6d29fd4279773b7bbafd28afdb82
Ken McGaugh <ken@mcgaugh.co.uk>
2025-11-01 11:14:31 +1300

    Feature to allow for custom thumbnail images.
    Signed-off-by: Ken McGaugh <ken@mcgaugh.co.uk>
    Signed-off-by: Ted Waine <ted.waine@gmail.com>

--- include/xstudio/media/enums.hpp
+++ include/xstudio/media/enums.hpp
@@ -4,7 +4,7 @@
 namespace xstudio {
 namespace media {
 
-    typedef enum { MT_IMAGE = 0x1L, MT_AUDIO = 0x2L } MediaType;
+    typedef enum { MT_IMAGE = 0x1L, MT_AUDIO = 0x2L, MT_THUMBNAIL = 0x3L } MediaType;
     typedef enum {
         MS_ONLINE      = 0x0L,
         MS_MISSING     = 0x01L,
