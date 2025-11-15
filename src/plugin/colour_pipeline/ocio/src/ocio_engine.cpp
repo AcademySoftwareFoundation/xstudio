63281b9b3a55a8e6fe12c4d0a81141734ac8e1c0
Ken McGaugh <ken@mcgaugh.co.uk>
2025-11-15 13:40:26 +1300

    Modified OCIOEngine::get_ocio_config() to pass the "ocio_config" metadata value through xstudio's file-path remapping mechanism.  Likewise, all the search paths within the config are also passed through file path remapping.
    Note that any path set via the OCIO environment variable is left as-is.
    
    Signed-off-by: Ken McGaugh <ken@mcgaugh.co.uk>
    Signed-off-by: Sam.Richards@taurich.org <Sam.Richards@taurich.org>

--- src/plugin/colour_pipeline/ocio/src/ocio_engine.cpp
+++ src/plugin/colour_pipeline/ocio/src/ocio_engine.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 #include "ocio_engine.hpp"
 
+#include "xstudio/utility/helpers.hpp"
 #include "xstudio/utility/string_helpers.hpp"
 #include "xstudio/ui/opengl/shader_program_base.hpp"
 #include "xstudio/thumbnail/thumbnail.hpp"
@@ -328,7 +329,8 @@ OCIO::ConstConfigRcPtr
 OCIOEngine::get_ocio_config(const utility::JsonStore &src_colour_mgmt_metadata) const {
 
     const std::string config_name =
-        src_colour_mgmt_metadata.get_or("ocio_config", default_config_);
+        utility::forward_remap_file_path(
+            src_colour_mgmt_metadata.get_or("ocio_config", default_config_));
     const std::string displays =
         src_colour_mgmt_metadata.get_or("active_displays", std::string(""));
     const std::string views  = src_colour_mgmt_metadata.get_or("active_views", std::string(""));
@@ -378,6 +380,14 @@ OCIOEngine::get_ocio_config(const utility::JsonStore &src_colour_mgmt_metadata) 
         econfig->setActiveDisplays(displays.c_str());
     if (!views.empty())
         econfig->setActiveViews(views.c_str());
+
+    econfig->clearSearchPaths();
+    for (int i = 0; i < config->getNumSearchPaths(); ++i) {
+        auto path = std::string(config->getSearchPath(i));
+        auto newpath = utility::forward_remap_file_path(path);
+        econfig->addSearchPath(newpath.c_str());
+    }
+
     config                     = econfig;
     ocio_config_cache_[concat] = config;
 
